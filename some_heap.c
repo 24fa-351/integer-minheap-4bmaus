2ee8fc7c52bbc3db109ddf5d7281b3ca6b789b8e
4bmaus <111831442+4bmaus@users.noreply.github.com>
2024-10-02 18:56:56 -0700

    Update some_heap.c

--- some_heap.c
+++ some_heap.c
@@ -1,71 +1,28 @@
+#ifndef HEAP_H
+#define HEAP_H
 
-#include <stdio.h>
-#include <stdlib.h>
+// Min heap definitions
+typedef unsigned long long heap_key_t;
+typedef void* heap_value_t;
+#define HEAP_KEY_FORMAT "%llu"
 
-#include "heap.h"
+typedef struct {
+    heap_key_t key;
+    heap_value_t value;
+} heap_node_t;
 
-#define KEY_NOT_PRESENT -1
+typedef struct {
+    heap_node_t *data;
+    int size;
+    int capacity;
+} heap_t;
 
-heap_t *heap_create(int capacity) {
-}
+// Function declarations
+heap_t *heap_create(int capacity);
+void heap_free(heap_t *heap);
+unsigned int heap_size(heap_t *heap);
+void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data);
+heap_value_t heap_remove_min(heap_t *heap);
+void heap_print(heap_t *heap);
 
-void heap_free(heap_t *heap) {
-}
-
-unsigned int heap_size(heap_t *heap) { return heap->size; }
-
-unsigned int heap_parent(unsigned int index) {  }
-
-unsigned int heap_left_child(unsigned int index) { }
-
-unsigned int heap_right_child(unsigned int index) {  }
-
-unsigned int heap_level(unsigned int index) {}
-
-void heap_print(heap_t *heap) {
-    for (int ix = 0; ix < heap_size(heap); ix++) {
-        printf("%3d - %3d : " HEAP_KEY_FORMAT "\n", heap_level(ix), ix,
-               heap->data[ix].key);
-    }
-    printf("\n");
-}
-
-void heap_swap(heap_t *heap, int index1, int index2) {
-}
-
-void heap_bubble_up(heap_t *heap, int index) {
-}
-
-void heap_bubble_down(heap_t *heap, int index) {
-}
-
-void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data) {
-    if (heap_size(heap) == heap->capacity) {
-        return;
-    }
-
-    heap->data[heap_size(heap)].key = key;
-    heap->data[heap_size(heap)].value = data;
-    heap->size++;
-
-    heap_bubble_up(heap, heap_size(heap) - 1);
-}
-
-
-heap_value_t heap_remove_min(heap_t *heap) {
-    if (heap_size(heap) == 0) {
-        return NULL;
-    }
-
-    heap_value_t min = heap->data[0].value;
-
-    heap->size--;
-
-    // move last element to the root
-    heap->data[0] = heap->data[heap_size(heap)];
-
-    // then bubble it down to its correct position
-    heap_bubble_down(heap, 0);
-
-    return min;
-}
+#endif
