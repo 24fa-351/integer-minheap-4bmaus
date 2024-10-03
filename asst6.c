6163d5987b0fe366f337de4638068086d9a7132e
4bmaus <111831442+4bmaus@users.noreply.github.com>
2024-10-02 19:08:47 -0700

    Update asst6.c

--- asst6.c
+++ asst6.c
@@ -10,7 +10,7 @@ unsigned long long rand_between(unsigned long long min, unsigned long long max) 
 
 void test_heap(void) {
     heap_t *heap = heap_create(200);
-    FILE *output = fopen("heap_output.txt", "w");
+    FILE *output = fopen("heap_output.txt", "w");  // Open file for writing
 
     if (!output) {
         printf("Error opening file!\n");
@@ -20,18 +20,19 @@ void test_heap(void) {
     for (heap_key_t ix = 0; ix < 20; ix++) {
         heap_key_t key = rand_between(0, 1000);
         heap_insert(heap, key, (heap_value_t)key);
-        fprintf(output, "Inserted %llu\n", key);
-        heap_print(heap);
+        fprintf(output, "Added %llu\n", key);  // Write to file instead of printing to console
+        heap_print_to_file(heap, output);  // Custom print function for writing to file
     }
-
+    
+    // Removing 10 elements
     for (int ix = 0; ix < 10; ix++) {
         heap_key_t key = (heap_key_t)heap_remove_min(heap);
-        fprintf(output, "Removed %llu\n", key);
-        heap_print(heap);
+        fprintf(output, "Removed %llu\n", key);  // Write to file
+        heap_print_to_file(heap, output);  // Custom print function for writing to file
     }
 
-    fclose(output);
-    heap_free(heap);
+    fclose(output);  // Close the file when done
+    heap_free(heap);  // Free the heap memory
 }
 
 int main(int argc, char *argv[]) {
