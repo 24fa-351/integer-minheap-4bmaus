e89f13166f8078e564a336860c846d48ab61eaa9
4bmaus <111831442+4bmaus@users.noreply.github.com>
2024-12-15 14:17:24 -0800

    Update some_heap.h

--- some_heap.h
+++ some_heap.h
@@ -3,18 +3,18 @@
 
 // Min heap definitions
 typedef unsigned long long heap_key_t;
-typedef void* heap_value_t;
+typedef void *heap_value_t;
 #define HEAP_KEY_FORMAT "%llu"
 
 typedef struct {
-    heap_key_t key;
-    heap_value_t value;
+  heap_key_t key;
+  heap_value_t value;
 } heap_node_t;
 
 typedef struct {
-    heap_node_t *data;
-    int size;
-    int capacity;
+  heap_node_t *data;
+  int size;
+  int capacity;
 } heap_t;
 
 // Function declarations
